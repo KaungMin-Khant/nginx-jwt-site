131b9f10bfe343b3908ad703c0ef84c085dfebd6
kaungmin-khant <kaungminkhant@ackaya.com>
2024-01-11 13:16:24 +0630

    base code

---
+++ auth/login.js
@@ -0,0 +1,13 @@
+async function onSubmit(event) {
+  event.preventDefault();
+  // const response = await fetch('http://127.0.0.1:80/login', {
+  //   method: 'POST',
+  // })
+  // const data = await response.json();
+  // if (data.token) {
+    // localStorage.setItem('token', JSON.stringify({
+    //   token: data.token
+    // }))
+    window.location.href = "/product"
+  // }
+}
